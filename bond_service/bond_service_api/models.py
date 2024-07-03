87bf05e5658e777c73fa5ebf427e8c57d0af79b7
Apti <aptia@thinkeasy.cz>
2024-07-03 23:14:18 +0200

    add first proposal of models

--- bond_service/bond_service_api/models.py
+++ bond_service/bond_service_api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class YieldsFrequencyChoices(models.TextChoices):
+    MONTHLY = 12, 'Monthly'
+    QUARTERLY = 4, 'Quarterly'
+    ANNUALLY = 1, 'Annually'
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Bond(models.Model):
+    emission_name = models.CharField(max_length=50)
+    emission_isin = models.CharField(max_length=50)
+    bond_value = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    purchase_date = models.DateField(null=True, blank=True)
+    maturity_date = models.DateField(null=True, blank=True)
+    yields_frequency = models.IntegerField(
+        choices=YieldsFrequencyChoices.choices,
+        default=YieldsFrequencyChoices.MONTHLY,
+        null=True,
+        blank=True
+    )
+    portfolio = models.ForeignKey(Portfolio, related_name='portfolios', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.emission_name
