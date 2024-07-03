4bfa9b081f52646441c87d2a4179120cf720f08e
Apti <aptia@thinkeasy.cz>
2024-07-03 23:15:07 +0200

    add basic views for tokens and users

--- bond_service/bond_service_api/views.py
+++ bond_service/bond_service_api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView
+)
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+# from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+# from drf_spectacular.types import OpenApiTypes
+from .serializers import (
+    MyTokenObtainPairSerializer,
+    RegisterSerializer,
+    UsersListSerializer,
+)
 
-# Create your views here.
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    permission_classes = [AllowAny]
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class UsersListView(ListAPIView):
+    serializer_class = UsersListSerializer
+    queryset = User.objects.all().order_by('pk')
