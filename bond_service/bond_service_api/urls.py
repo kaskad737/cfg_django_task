6f275bb8aef976992601628234bcab86abc0b6ac
Apti <aptia@thinkeasy.cz>
2024-07-04 20:12:09 +0200

    add urls

--- bond_service/bond_service_api/urls.py
+++ bond_service/bond_service_api/urls.py
@@ -2,19 +2,21 @@ from django.urls import path
 
 from .views import (
     CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
     RegisterView,
     UsersListView,
-)
-
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-    TokenVerifyView,
+    PortfolioListCreateView,
+    PortfolioInvestmentAnalysisView,
 )
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
+    path('user_register/', RegisterView.as_view(), name='user_register'),
     path('users/', UsersListView.as_view(), name='users'),
+    path("portfolio/", PortfolioListCreateView.as_view(), name='portfolio'),
+    path('portfolio_investment_analysis/', PortfolioInvestmentAnalysisView.as_view(),
+         name='portfolio_investment_analysis'),
 ]
