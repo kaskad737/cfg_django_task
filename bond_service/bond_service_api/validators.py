46c00f11f32e18bdbb9637ea873cad0d8833d537
Apti <aptia@thinkeasy.cz>
2024-07-04 09:24:21 +0200

    add validate_isin validator

---
+++ bond_service/bond_service_api/validators.py
@@ -0,0 +1,16 @@
+import requests
+from django.core.exceptions import ValidationError
+
+
+def validate_isin(value):
+    url = f'https://www.cdcp.cz/isbpublicjson/api/VydaneISINy?isin={value}'
+    response = requests.get(url)
+
+    if response.ok:
+        data = response.json()
+        if not data.get('vydaneisiny'):
+            raise ValidationError(f'ISIN {value} is not found in the central depository.')
+        else:
+            return value
+    else:
+        raise ValidationError(f'API request error: {response.status_code}')
