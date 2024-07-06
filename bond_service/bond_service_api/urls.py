b2cda24cfc35447acf913af0486a9bb36a9d0fd0
Apti <aptia@thinkeasy.cz>
2024-07-06 20:13:50 +0200

    add urls for views

--- bond_service/bond_service_api/urls.py
+++ bond_service/bond_service_api/urls.py
@@ -4,23 +4,27 @@ from .views import (
     CustomTokenObtainPairView,
     CustomTokenRefreshView,
     CustomTokenVerifyView,
-    RegisterView,
+    UserRegisterView,
     UsersListView,
+    UserRetrieveUpdateDestroyView,
     PortfolioListCreateView,
+    PortfolioRetrieveUpdateDestroyView,
     PortfolioInvestmentAnalysisView,
-    ListCreateBondView,
-    RetrieveUpdateDestroyBondView,
+    BondListCreateView,
+    BondRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
-    path('user_register/', RegisterView.as_view(), name='user_register'),
+    path('user_register/', UserRegisterView.as_view(), name='user_register'),
     path('users/', UsersListView.as_view(), name='users'),
+    path('users/<uuid:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user_details'),
     path('portfolio/', PortfolioListCreateView.as_view(), name='portfolio'),
+    path('portfolio/<uuid:pk>/', PortfolioRetrieveUpdateDestroyView.as_view(), name='portfolio_details'),
     path('portfolio_investment_analysis/', PortfolioInvestmentAnalysisView.as_view(),
          name='portfolio_investment_analysis'),
-    path('bonds/', ListCreateBondView.as_view(), name='bond'),
-    path('bonds/<int:pk>/', RetrieveUpdateDestroyBondView.as_view(), name='bond_details'),
+    path('bonds/', BondListCreateView.as_view(), name='bond'),
+    path('bonds/<uuid:pk>/', BondRetrieveUpdateDestroyView.as_view(), name='bond_details'),
 ]
