15ab2b99170fcd0c3041a97452251b30268ea490
Apti <aptia@thinkeasy.cz>
2024-07-07 23:41:55 +0200

    add test test_user_cant_create_bond_in_his_portfolio_with_bad_emission_isin

--- bond_service/bond_service_api/tests/test_bonds.py
+++ bond_service/bond_service_api/tests/test_bonds.py
@@ -93,6 +93,29 @@ def test_user_cant_create_bond_not_in_his_portfolio(
 
 
 @pytest.mark.django_db
+def test_user_cant_create_bond_in_his_portfolio_with_bad_emission_isin(
+    api_client: APIClient,
+    authenticate_user,
+    user1: User,
+    portfolio1: Portfolio
+):
+    list_bonds_url = reverse('bond_service_api:bond')
+    authenticate_user(username='user1', password='password1')
+    data = {
+        'emission_name': 'test_emision_name4',
+        'emission_isin': 'CZ0001004115',
+        'bond_value': '105',
+        'interest_rate': '15.00',
+        'purchase_date': '2024-07-06',
+        'maturity_date': '2024-08-06',
+        'yields_frequency': 12,
+        'portfolio': portfolio1.pk
+    }
+    response: Response = api_client.post(list_bonds_url, data=data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
 def test_user_can_get_his_bond(
         api_client: APIClient,
         authenticate_user,
