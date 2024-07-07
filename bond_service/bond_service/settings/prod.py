9e8e6741837261e5aac684e616d66b1c2f9f811d
Apti <aptia@thinkeasy.cz>
2024-07-07 22:50:00 +0200

    add files for prod env

--- bond_service/bond_service/settings/prod.py
+++ bond_service/bond_service/settings/prod.py
@@ -1,29 +1,30 @@
-import os
-import sys
 from datetime import timedelta
 
 from .base import *  # noqa
 
 SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(minutes=30)}
 
-ALLOWED_HOSTS += os.getenv("PROD_URL").split()  # noqa
-
-# trusted origins for rest framework (using django-cors-headers)
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_URL").split()
-# trusted origins for django admin
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split()
-CSRF_COOKIE_SECURE = True
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {"console": {"class": "logging.StreamHandler", "stream": sys.stderr}},  # noqa
-    "loggers": {
-        "django.request": {
-            "handlers": ["console"],
-            "level": "ERROR",  # change debug level as appropriate
-            "propagate": True,
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'base': {
+            'format': '%(asctime)s.%(msecs)03d | %(name)-25s | %(levelname)-9s | %(lineno)-4d | %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
         }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'base'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
     },
 }
 
--- run_prod.sh
+++ run_prod.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_FILE="docker-compose.local.yml"
+DOCKER_COMPOSE_FILE="docker-compose.prod.yml"
 
 if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
     echo "Error: Docker Compose file '$DOCKER_COMPOSE_FILE' not found."
     exit 1
 fi
 
-ENV_FILE=".local"
+ENV_FILE=".prod"
 
 if [ -f "$ENV_FILE" ]; then
     echo "Loading environment variables from $ENV_FILE"
     export $(grep -v '^#' $ENV_FILE | xargs)
 else
-    echo "Error: .local file '$ENV_FILE' for Docker Compose not found."
+    echo "Error: .prod file '$ENV_FILE' for Docker Compose not found."
     exit 1
 fi
 
-docker-compose -f "$DOCKER_COMPOSE_FILE" up
+docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
