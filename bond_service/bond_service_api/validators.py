81425d10dbeb4dd783d79d855ac5d368d66d3072
Apti <aptia@thinkeasy.cz>
2024-07-07 21:45:28 +0200

    refactor

--- bond_service/bond_service_api/validators.py
+++ bond_service/bond_service_api/validators.py
@@ -5,7 +5,6 @@ from django.core.exceptions import ValidationError
 def validate_isin(value):
     url = f'https://www.cdcp.cz/isbpublicjson/api/VydaneISINy?isin={value}'
     response = requests.get(url)
-
     if response.ok:
         data = response.json()
         if not data.get('vydaneisiny'):
