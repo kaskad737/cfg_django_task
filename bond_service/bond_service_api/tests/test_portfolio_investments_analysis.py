169d8b188a0871524a3f9b9e2d264fa0133d8958
Apti <aptia@thinkeasy.cz>
2024-07-08 08:11:00 +0200

    fix tests

--- bond_service/bond_service_api/tests/test_portfolio_investments_analysis.py
+++ bond_service/bond_service_api/tests/test_portfolio_investments_analysis.py
@@ -7,6 +7,7 @@ from requests import Response
 from ..models import Portfolio, Bond
 from decimal import Decimal
 from django.db.models.query import QuerySet
+from django.utils import timezone
 
 
 @pytest.mark.django_db
@@ -17,14 +18,19 @@ def test_user_get_his_portfolio_analysis(
     portfolio1: Portfolio
 ):
     all_portfolio_bonds: QuerySet[Bond] = portfolio1.bonds.all()
+    total_value = Decimal(sum(all_portfolio_bonds.values_list('bond_value', flat=True)))
+    avg_interest_rate = Decimal(sum(all_portfolio_bonds.values_list(
+        'interest_rate', flat=True)) / all_portfolio_bonds.count())
     nearest_maturity_bond: Bond = min(all_portfolio_bonds, key=lambda bond: bond.maturity_date)
+    years = Decimal((nearest_maturity_bond.maturity_date - timezone.now().date()).days / Decimal(365.25))
+    future_value = total_value * (1 + avg_interest_rate / 100) ** years
     list_portfolios_url = reverse('bond_service_api:portfolio_investment_analysis')
     authenticate_user(username='user1', password='password1')
     response: Response = api_client.get(list_portfolios_url, data={'portfolio_pk': portfolio1.pk})
-    assert response.data['average_interest_rate'] == Decimal('15.00')
+    assert response.data['average_interest_rate'] == avg_interest_rate
     assert response.data['nearest_maturity_bond']['emission_name'] == nearest_maturity_bond.emission_name
-    assert response.data['total_value'] == Decimal('200.00')
-    assert response.data['future_value'] == Decimal('202.3091123419387225004762924')
+    assert response.data['total_value'] == total_value
+    assert response.data['future_value'] == future_value
     assert response.status_code == status.HTTP_200_OK
 
 
@@ -49,12 +55,17 @@ def test_admin_get_portfolio_analysis(
     portfolio1: Portfolio
 ):
     all_portfolio_bonds: QuerySet[Bond] = portfolio1.bonds.all()
+    total_value = Decimal(sum(all_portfolio_bonds.values_list('bond_value', flat=True)))
+    avg_interest_rate = Decimal(sum(all_portfolio_bonds.values_list(
+        'interest_rate', flat=True)) / all_portfolio_bonds.count())
     nearest_maturity_bond: Bond = min(all_portfolio_bonds, key=lambda bond: bond.maturity_date)
+    years = Decimal((nearest_maturity_bond.maturity_date - timezone.now().date()).days / Decimal(365.25))
+    future_value = total_value * (1 + avg_interest_rate / 100) ** years
     list_portfolios_url = reverse('bond_service_api:portfolio_investment_analysis')
     authenticate_user(username='admin', password='adminpassword')
     response: Response = api_client.get(list_portfolios_url, data={'portfolio_pk': portfolio1.pk})
-    assert response.data['average_interest_rate'] == Decimal('15.00')
+    assert response.data['average_interest_rate'] == avg_interest_rate
     assert response.data['nearest_maturity_bond']['emission_name'] == nearest_maturity_bond.emission_name
-    assert response.data['total_value'] == Decimal('200.00')
-    assert response.data['future_value'] == Decimal('202.3091123419387225004762924')
+    assert response.data['total_value'] == total_value
+    assert response.data['future_value'] == future_value
     assert response.status_code == status.HTTP_200_OK
